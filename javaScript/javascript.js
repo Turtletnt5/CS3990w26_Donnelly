c9c6043186e52f194b429c3ed76947fc3d7d4faa
Turtletnt5 <mdturtleman@gmail.com>
2026-02-24 14:16:55 -0700

    stopping for today

--- javaScript/javascript.js
+++ javaScript/javascript.js
@@ -1,4 +1,8 @@
-var r = document.querySelector(':root');
+let r = document.querySelector(':root');
+
+let rgbMode = false
+
+
 
 function setPageToDarkMode(){
     r.style.setProperty('--background_colour', 'rgb(9, 24, 31)');
@@ -11,6 +15,18 @@ function setPageToLightMode(){
     r.style.setProperty('--font_colour', 'rgba(0, 0, 0, 1)');
     r.style.setProperty('--border_colour', 'rgba(0, 0, 0, 1)');
 }
+
+function setPageToRGBMode(){
+    if(rgbMode){
+    r.style.setProperty('--background_colour', 'rgba(255, 255, 255, 1)');
+    r.style.setProperty('--font_colour', 'rgba(0, 0, 0, 1)');
+    r.style.setProperty('--border_colour', 'rgba(0, 0, 0, 1)');
+    }
+}
+
+setInterval()
+
+
 
 //--background_colour: rgb(9, 24, 31);
 //--font_colour: rgb(255, 255, 255);
